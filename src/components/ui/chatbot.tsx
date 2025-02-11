8e6fa9f9393f50909fdae4a2cc19a45f12f0002c
abir <abir4dev@gmail.com>
2025-02-12 00:17:10 +0600

    feat: implemented open-ai-integration

--- src/components/ui/chatbot.tsx
+++ src/components/ui/chatbot.tsx
@@ -3,16 +3,20 @@ import { useState, useRef, useEffect } from "react";
 import { X, BotMessageSquare, Send } from "lucide-react";
 import { motion } from "framer-motion";
 
+import { useChat } from "@ai-sdk/react";
+
 const ChatBot: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [messages, setMessages] = useState<
-    { text: string; sender: "bot" | "user" }[]
-  >([
-    {
-      text: "Hello! I'm Nebu, your friendly chatbot from the BRAC University Computer Club (BUCC)! 🤖🎉 I'm here to help you explore all the exciting activities, fun engagements, and anything related to our university. Whether it's about our club events, courses, or just some general info, feel free to ask me anything! 😊",
-      sender: "bot",
-    },
-  ]);
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+
+  // const [messages, setMessages] = useState<
+  //   { text: string; sender: "bot" | "user" }[]
+  // >([
+  //   {
+  //     text: "Hello! I'm Nebu, your friendly chatbot from the BRAC University Computer Club (BUCC)! 🤖🎉 I'm here to help you explore all the exciting activities, fun engagements, and anything related to our university. Whether it's about our club events, courses, or just some general info, feel free to ask me anything! 😊",
+  //     sender: "bot",
+  //   },
+  // ]);
   const messagesEndRef = useRef<HTMLDivElement>(null);
 
   const toggleChat = () => setIsOpen(!isOpen);
@@ -25,26 +29,26 @@ const ChatBot: React.FC = () => {
     scrollToBottom();
   }, [messages]);
 
-  const handleSendMessage = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const input = e.currentTarget.elements.namedItem(
-      "message",
-    ) as HTMLInputElement;
-    if (input.value.trim()) {
-      setMessages([...messages, { text: input.value, sender: "user" }]);
-      input.value = "";
+  // const handleSendMessage = (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   const input = e.currentTarget.elements.namedItem(
+  //     "message",
+  //   ) as HTMLInputElement;
+  //   if (input.value.trim()) {
+  //     setMessages([...messages, { text: input.value, sender: "user" }]);
+  //     input.value = "";
 
-      setTimeout(() => {
-        setMessages((prevMessages) => [
-          ...prevMessages,
-          {
-            text: "Oops! Looks like our chatbot is on a little vacation 🏖️. It's getting a fresh update and taking a quick break to recharge. We promise it’ll be back better than ever! 💪✨",
-            sender: "bot",
-          },
-        ]);
-      }, 500);
-    }
-  };
+  //     setTimeout(() => {
+  //       setMessages((prevMessages) => [
+  //         ...prevMessages,
+  //         {
+  //           text: "Oops! Looks like our chatbot is on a little vacation 🏖️. It's getting a fresh update and taking a quick break to recharge. We promise it’ll be back better than ever! 💪✨",
+  //           sender: "bot",
+  //         },
+  //       ]);
+  //     }, 500);
+  //   }
+  // };
 
   return (
     <div className="fixed bottom-4 right-4 sm:bottom-6 sm:right-6">
@@ -73,27 +77,22 @@ const ChatBot: React.FC = () => {
           <div className="flex-1 space-y-2 overflow-y-auto p-3 text-sm">
             {messages.map((msg, index) => (
               <div
-                key={index}
+                key={msg.id}
                 className={`flex ${
-                  msg.sender === "user" ? "justify-end" : "justify-start"
+                  msg.role === "user" ? "justify-end" : "justify-start"
                 }`}
               >
-                {msg.sender === "bot" && (
-                  <div
-                    className="mr-2 flex h-8 w-8 items-center justify-center rounded-full bg-gradient-to-r from-blue-400 to-purple-600"
-                    // This div is the avatar, add any bot image or leave it as a gradient circle
-                  >
-                    <span className="text-xl text-white">🤖</span>
-                  </div>
+                {msg.role === "assistant" && (
+                  <div className="mr-2 flex h-8 w-8 items-center justify-center rounded-full bg-gradient-to-r from-yellow-400 via-orange-500 to-yellow-600"></div>
                 )}
                 <div
                   className={`max-w-[75%] rounded-lg px-4 py-2 ${
-                    msg.sender === "user"
+                    msg.role === "user"
                       ? "bg-blue-600 text-white"
                       : "bg-gray-800 text-gray-300"
                   }`}
                 >
-                  {msg.text}
+                  {msg.content}
                 </div>
               </div>
             ))}
@@ -102,13 +101,15 @@ const ChatBot: React.FC = () => {
 
           <div className="sticky bottom-0 border-t border-gray-700 bg-gray-900 p-3">
             <form
-              onSubmit={handleSendMessage}
+              onSubmit={handleSubmit}
               className="flex items-center space-x-2"
             >
               <input
                 type="text"
                 name="message"
+                value={input}
                 placeholder="Type a message..."
+                onChange={handleInputChange}
                 className="flex-1 rounded-md border border-gray-700 bg-gray-800 px-3 py-2 text-sm text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
               <button
