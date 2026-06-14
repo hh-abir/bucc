c4c6acdc44d4c5f1bc437c4d799d7312ac7e70d1
Ahmed Unais <165322341+ahmedunais@users.noreply.github.com>
2026-06-14 22:32:16 +0600

    Update intake and evaluation active flags

--- src/constants/buccInfo.ts
+++ src/constants/buccInfo.ts
@@ -3,8 +3,8 @@ const intakeInfo = {
   intakeName: "Spring 2026",
   intakeStartDate: "2026-02-22",
   intakeEndDate: "2026-03-10",
-  isIntakeActive: false,
-  isEvaluationActive: true,
+  isIntakeActive: true,
+  isEvaluationActive: false,
 };
 
 const buccSocials = {
